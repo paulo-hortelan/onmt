f6c64ef2c0b765bbc014ade0e9880f6c6ae36ad2
paulo-hortelan <paulo-hortelan@users.noreply.github.com>
2024-01-16 13:12:44 +0000

    Fix styling

--- src/Services/Zte300Service.php
+++ src/Services/Zte300Service.php
@@ -9,7 +9,8 @@ class Zte300Service
 {
     private Telnet $connection;
 
-    public function connect(Olt $olt, int $timeout = 3, int $streamTimeout = 3) {
+    public function connect(Olt $olt, int $timeout = 3, int $streamTimeout = 3)
+    {
         $this->connection = new Telnet($olt->host, 23, $timeout, $streamTimeout);
         $this->connection->stripPromptFromBuffer(true);
         $this->connection->login($olt->username, $olt->password, 'zte300');
@@ -17,27 +18,29 @@ class Zte300Service
         return $this;
     }
 
-    public function ontOpticalPower($interface): float{        
+    public function ontOpticalPower($interface): float
+    {
         $response = $this->connection->exec("show pon power attenuation $interface");
 
-        if(preg_match('/down.*Rx:(.*)\(dbm\)/m', $response, $match)){
+        if (preg_match('/down.*Rx:(.*)\(dbm\)/m', $response, $match)) {
             $opticalPower = (float) $match[1];
         } else {
             throw new \Exception('Ont optical power not found.');
         }
 
-        return $opticalPower;      
+        return $opticalPower;
     }
 
-    public function ontInterface($serial): string{        
+    public function ontInterface($serial): string
+    {
         $response = $this->connection->exec("show gpon onu by sn $serial");
 
-        if(preg_match('/gpon-onu.*/m', $response, $match)){
+        if (preg_match('/gpon-onu.*/m', $response, $match)) {
             $interface = (string) $match[0];
         } else {
             throw new \Exception('Ont interface not found.');
         }
 
-        return $interface;       
+        return $interface;
     }
 }
