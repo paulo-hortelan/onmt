a25fb2450845bcd6a9892883fd967e1abf7d46ad
paulo-hortelan <paulo.ribeiro@aonet.com.br>
2024-01-22 14:24:54 -0300

    Add ceo model

---
+++ database/migrations/create_ceos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('ceos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+
+            $table->foreignId('dio_id')->constrained(
+                table: 'dios',
+                indexName: 'ceos_dio_id'
+            )->onUpdate('cascade')->onDelete('cascade');
+
+            $table->timestamps();            
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('ceos');
+    }
+};
---
+++ src/Models/Ceo.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace PauloHortelan\OltMonitoring\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+/**
+ * PauloHortelan\OltMonitoring\Models\Ceo
+ *
+ * @property string $name
+ * @property integer $olt_id
+ */
+class Ceo extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'dio_id',
+    ];
+
+    /**
+     * Get the DIO associated with the CEO.
+     */
+    public function dio()
+    {
+        return $this->belongsTo(Dio::class);
+    }
+
+    /**
+     * Get the CEO Splitters from the associated CEO
+     */
+    public function ceo_splitters(): HasMany
+    {
+        return $this->hasMany(CeoSplitter::class);
+    }
+}
