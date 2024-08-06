47b333b43b2c34b77dc2a5c57ebe1e3263b14db9
paulo-hortelan <paulo.ribeiro@aonet.com.br>
2024-08-06 09:51:35 -0300

    fix: provider

--- src/OnmtServiceProvider.php
+++ src/OnmtServiceProvider.php
@@ -18,40 +18,8 @@ class OnmtServiceProvider extends PackageServiceProvider
     public function boot()
     {
         parent::boot();
-
-        $this->registerRoutes();
-
-        $this->publishes([
-            __DIR__.'/../routes/onmt.php' => base_path('routes/onmt.php'),
-        ], 'onmt-routes');
-
-        $this->publishes([
-            __DIR__.'/../database/migrations/' => database_path('migrations/onmt'),
-        ], 'onmt-migrations');
-
-        $this->publishes([
-            __DIR__.'/../database/migrations/' => database_path('migrations/onmt'),
-        ], 'onmt-models');
-
-        Factory::guessFactoryNamesUsing(function (string $modelName) {
-            return 'PauloHortelan\\Onmt\\Database\\Factories\\'.class_basename($modelName).'Factory';
-        });
     }
 
-    protected function registerRoutes(): void
-    {
-        Route::group($this->routeConfiguration(), function () {
-            $this->loadRoutesFrom(__DIR__.'/../routes/onmt.php');
-        });
-    }
-
-    protected function routeConfiguration(): array
-    {
-        return [
-            'prefix' => config('onmt.prefix'),
-            'middleware' => config('onmt.middleware'),
-        ];
-    }
 
     public function configurePackage(Package $package): void
     {
@@ -61,14 +29,6 @@ class OnmtServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('onmt')
-            ->hasConfigFile('onmt')
-            ->hasMigration('create_olts_table')
-            ->hasInstallCommand(function (InstallCommand $command) {
-                $command
-                    ->publishConfigFile()
-                    ->publishMigrations()
-                    ->askToRunMigrations();
-            });
+            ->name('onmt');
     }
 }
