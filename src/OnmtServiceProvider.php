29e964b74c7d52b9db388bb1ab928b2fffaaf08d
Paulo <paulo.ribeiro@aonet.com.br>
2024-12-05 17:56:01 -0300

    chore: rem comments

--- src/OnmtServiceProvider.php
+++ src/OnmtServiceProvider.php
@@ -12,11 +12,6 @@ class OnmtServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('onmt')
             ->hasConfigFile();
