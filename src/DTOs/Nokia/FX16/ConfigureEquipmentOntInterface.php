1a06b60bf8cb96598025d8fdbfbe5a55a7d4b46f
Paulo <paulo.ribeiro@aonet.com.br>
2025-05-05 08:04:36 -0300

    fix: nokia

--- src/DTOs/Nokia/FX16/ConfigureEquipmentOntInterface.php
+++ src/DTOs/Nokia/FX16/ConfigureEquipmentOntInterface.php
@@ -53,39 +53,34 @@ class ConfigureEquipmentOntInterface
     public function buildCommand(): string
     {
         $commandParts = [];
+        $propertiesMap = [
+            'plannedUsRate' => 'planned-us-rate',
+            'subslocid' => 'subslocid',
+            'fecUp' => 'fec-up',
+            'swVerPlnd' => 'sw-ver-pland',
+            'opticsHist' => 'optics-hist',
+            'plandCfgfile1' => 'pland-cfgfile1',
+            'desc1' => 'desc1',
+        ];
 
-        $formattedSerial = $this->sernum ? substr_replace($this->sernum, ':', 4, 0) : null;
-
-        if (isset($this->swVerPlnd)) {
-            $commandParts[] = "sw-ver-pland {$this->swVerPlnd}";
+        foreach ($propertiesMap as $property => $command) {
+            if (isset($this->$property)) {
+                $commandParts[] = "{$command} {$this->$property}";
+            }
         }
 
-        if ($this->swDnloadVersion === self::NO) {
-            $commandParts[] = 'no sw-dnload-version';
-        } elseif (isset($this->swDnloadVersion)) {
-            $commandParts[] = "sw-dnload-version {$this->swDnloadVersion}";
-        }
-
-        if (isset($formattedSerial)) {
+        // Handle special cases
+        if (isset($this->sernum)) {
+            $formattedSerial = substr_replace($this->sernum, ':', 4, 0);
             $commandParts[] = "sernum {$formattedSerial}";
         }
 
-        if (isset($this->opticsHist)) {
-            $commandParts[] = "optics-hist {$this->opticsHist}";
+        if (isset($this->swDnloadVersion)) {
+            $commandParts[] = $this->swDnloadVersion === self::NO ? 'no sw-dnload-version' : "sw-dnload-version {$this->swDnloadVersion}";
         }
 
-        if (isset($this->plandCfgfile1)) {
-            $commandParts[] = "pland-cfgfile1 {$this->plandCfgfile1}";
-        }
-
-        if ($this->dnloadCfgfile1 === self::NO) {
-            $commandParts[] = 'no dnload-cfgfile1';
-        } elseif (isset($this->dnloadCfgfile1)) {
-            $commandParts[] = "dnload-cfgfile1 {$this->dnloadCfgfile1}";
-        }
-
-        if (isset($this->desc1)) {
-            $commandParts[] = "desc1 {$this->desc1}";
+        if (isset($this->dnloadCfgfile1)) {
+            $commandParts[] = $this->dnloadCfgfile1 === self::NO ? 'no dnload-cfgfile1' : "dnload-cfgfile1 {$this->dnloadCfgfile1}";
         }
 
         return implode(' ', $commandParts);
